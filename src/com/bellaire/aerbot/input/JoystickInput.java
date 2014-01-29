b9a640a298517df601945c2b370146cec2d101b3
Michael Murphey <anonymous9304@gmail.com>
2014-01-29 15:00:27 -0600

    Updated input classes with methods for buttons

--- src/com/bellaire/aerbot/input/JoystickInput.java
+++ src/com/bellaire/aerbot/input/JoystickInput.java
@@ -3,9 +3,9 @@ package com.bellaire.aerbot.input;
 import edu.wpi.first.wpilibj.Joystick;
 
 public class JoystickInput implements InputMethod {
-    
+
     private Joystick left, right;
-    
+
     public JoystickInput() {
         left = new Joystick(1);
         right = new Joystick(2);
@@ -18,7 +18,7 @@ public class JoystickInput implements InputMethod {
     public double getRightY() {
         return right.getY();
     }
-    
+
     public double getLeftX() {
         return left.getY();
     }
@@ -27,8 +27,24 @@ public class JoystickInput implements InputMethod {
         return right.getY();
     }
 
-    public boolean getButton(int button) {
-        return false;
+    public boolean getIntakeIn() {
+        return right.getRawButton(4);
     }
-    
+
+    public boolean getIntakeOut() {
+        return right.getRawButton(2);
+    }
+
+    public boolean getIntakePneumatic() {
+        return left.getRawButton(3);
+    }
+
+    public boolean getShoot() {
+        return right.getTrigger();
+    }
+
+    public boolean getAim() {
+        return left.getTrigger();
+    }
+
 }
