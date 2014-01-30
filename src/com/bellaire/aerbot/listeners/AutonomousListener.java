100254cfc74f18642ad873850435f872d0a79f13
Michael Murphey <anonymous9304@gmail.com>
2014-01-30 15:35:53 -0600

    minor change

--- src/com/bellaire/aerbot/listeners/AutonomousListener.java
+++ src/com/bellaire/aerbot/listeners/AutonomousListener.java
@@ -3,7 +3,6 @@ package com.bellaire.aerbot.listeners;
 import com.bellaire.aerbot.Environment;
 import com.bellaire.aerbot.systems.CameraSystem;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
-import edu.wpi.first.wpilibj.networktables2.client.NetworkTableClient;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutonomousListener implements Listener {
@@ -31,15 +30,15 @@ public class AutonomousListener implements Listener {
         /*if(!camera.foundTarget()) {
             env.getWheelSystem().drive(SmartDashboard.getNumber("Autonomous Turn Speed", 0.05), 1);
         }*/
-        NetworkTable client = NetworkTable.getTable("VisionTable");
+       /* NetworkTable client = NetworkTable.getTable("VisionTable");
         
         int cog_x = Integer.parseInt(client.getString("COG_X", "0"));
         int cog_y = Integer.parseInt(client.getString("COG_X", "0"));
         int img_width = Integer.parseInt(client.getString("IMAGE_WIDTH", "0"));
         int img_height = Integer.parseInt(client.getString("IMAGE_HEIGHT", "0"));
-        
-        
-        
+        int blob_count = Integer.parseInt(client.getString("BLOB_COUNT", "0"));
+        String blob = client.getString("BLOBS", null);
+        String blobs[] = blob.split(",");*/
         
     }
     
