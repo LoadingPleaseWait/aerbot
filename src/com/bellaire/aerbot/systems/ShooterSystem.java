67d1fa99cb0997c0e9b39233380a6891feb085e8
Michael Murphey <anonymous9304@gmail.com>
2014-02-03 17:27:28 -0600

    Beginning of ShooterSystem

--- src/com/bellaire/aerbot/systems/ShooterSystem.java
+++ src/com/bellaire/aerbot/systems/ShooterSystem.java
@@ -1,9 +1,54 @@
 package com.bellaire.aerbot.systems;
 
-/**
- *
- * @author Agro
- */
-public class ShooterSystem {
-    
+import com.bellaire.aerbot.Environment;
+import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.PIDSubsystem;
+
+public class ShooterSystem extends PIDSubsystem implements RobotSystem {
+
+    private static final double Kp = 6;
+    private static final double Ki = 0.0;
+    private static final double Kd = 0.0;
+    public static final double LAUNCH_SPEED = 3;
+    public static final double GRAVITY = 9.81;
+    public static final double GOAL_HEIGHT = 3;
+    private Environment env;
+    private AnalogChannel pot;
+    private Jaguar motors;
+
+    public ShooterSystem() {
+        super(Kp, Ki, Kd);
+    }
+
+    public void init(Environment e) {
+        env = e;
+        pot = new AnalogChannel(3);
+        motors = new Jaguar(3);
+    }
+
+    public void destroy() {
+        pot.free();
+        motors.free();
+    }
+
+    public void shoot(double distance) {
+        double angle = 0;
+        setSetpoint(0.0);
+        enable();
+    }
+
+    protected double returnPIDInput() {
+        return pot.getVoltage();
+    }
+
+    protected void usePIDOutput(double d) {
+        motors.set(d);
+    }
+
+    protected void initDefaultCommand() {
+
+    }
+
 }
